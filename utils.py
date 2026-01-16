44ec3b978c9a8761d910b327878081562b228be9
Alexey Volkov <alexey.volkov@bineon.team>
2026-01-16 19:11:43 +0100

    feat: update utility functions in `utils.py`

--- utils.py
+++ utils.py
@@ -219,7 +219,11 @@ def fetch_json_from_storage(omd_key: str, dest: str, filename: str):
     try:
         resp = requests.get(url, headers=headers, timeout=5)
         if resp.status_code == 200:
-            return resp.json()
+            try:
+                return resp.json()
+            except ValueError:
+                 logging.warning(f"Invalid JSON in {filename} from storage")
+                 return None
         elif resp.status_code == 404:
             return None
         else:
